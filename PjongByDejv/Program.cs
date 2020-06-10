21aa442395118fc4d952e8c6107d94725fe28123
David Welin <david.welin@outlook.com>
2020-06-10 22:06:17 +0200

    Test

--- PjongByDejv/Program.cs
+++ PjongByDejv/Program.cs
@@ -7,6 +7,7 @@ namespace PjongByDejv
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Pjong By Dejv");
         }
     }
 }

ea8dba4c55abd6f721a08207095d3b3dcef41846
David Welin <david.welin@outlook.com>
2020-06-10 22:03:09 +0200

    Add project files.

---
+++ PjongByDejv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PjongByDejv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
